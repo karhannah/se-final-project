1f96a29b309f004b2996879437b1ca2cb225a375
karhannah <karinnehannah@gmail.com>
2025-04-21 12:32:39 -0400

    Initial Commit

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,34 +1,16 @@
-import { useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import React from 'react';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
-import Preloader from "../Preloader/Preloader";
-import Navigation from "../Navigation/Navigation";
-import Header from "../Header/Header"
-
 function App() {
-	
+  const [count, setCount] = useState(0)
 
-	return (
-		<>
-			<div className = "root">
-				<div className = "page__content">
-					<Navigation />
-					<Header />
-
-					<BrowserRouter>
-						<Routes>
-							<Route path = "/" element = { <p></p> } />
-							<Route path = "/saved-news" element = {<p></p>} />
-						</Routes>
-					</BrowserRouter>
-
-					<Preloader />
-				</div>
-			</div>
-		</>
-	)
+  return (
+    <>
+		<p>test</p>
+    </>
+  )
 }
 
 export default App
