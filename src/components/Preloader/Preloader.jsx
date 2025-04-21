16b119cc05df7e43e8fad8b0770a1cefaa03ac65
karhannah <karinnehannah@gmail.com>
2025-04-21 12:43:25 -0400

    Added Nav bar and Header

---
+++ src/components/Preloader/Preloader.jsx
@@ -0,0 +1,14 @@
+import './Preloader.css';
+
+function Preloader() {
+	return (
+		<>
+			<div className = "preloader">
+				<div className = "circle-preloader"></div>
+				<h3>Searching for news...</h3>
+			</div>
+		</>
+	)
+}
+
+export default Preloader
